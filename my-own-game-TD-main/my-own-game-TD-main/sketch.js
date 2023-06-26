27d4f2c79664e78bb9415fe1e490b85b302d1ec8
shrmaaarna <123148550+shrmaaarna@users.noreply.github.com>
2023-06-26 19:03:44 +1000

    Add files via upload

---
+++ my-own-game-TD-main/my-own-game-TD-main/sketch.js
@@ -0,0 +1,22 @@
+
+var trex ,trex_running;
+var ground,groundImage
+
+function preload(){
+ 
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+
+ 
+}
+
+function draw(){
+  background("white");
+ 
+  
+drawSprites()
+}
